823fa059f6360587f549239895fe7dac3691132a
Andrew Baird <andrew.baird@housecallpro.com>
2019-12-24 11:02:47 -0700

    correctly calculating row and col values

--- sudoku.rb
+++ sudoku.rb
@@ -1,42 +1,49 @@
-class GameBoard
+class Solver
   attr_accessor :board
 
-  POSSIBLE_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  POSSIBLE_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
 
   def initialize
-    create_board
-  end
-
-  private
-
-  def create_board
     @board = Array.new(9){Array.new(9){Array.new(1)}}
   end
-end
 
-class Solver
-  def self.solve(game_board)
-
-  end
-
-  def self.compute_possible_values(row, column)
+  def compute_possible_values(row, column)
     found_values = []
 
-    #compute row
+    found_values.merge!(found_column_values(column))
+    found_values.merge!(found_row_values(row))
+    # found_values << found_nonet_values(row, column)
+    #solve for nonet values here
+
+    POSSIBLE_VALUES - found_values
+  end
+
+  def found_column_values(column)
+    values = []
+
     9.times do |index|
       if @board[index][column].length == 1 && !@board[index][column][0].nil?
-        found_values << @board[index][column][0]
+        values << @board[index][column][0]
       end
     end
 
-    #compute column
+    values
+  end
+
+  def found_row_values(row)
+    values = []
+
     9.times do |index|
       if  @board[row][index].length == 1 && !@board[row][index][0].nil?
-        found_values << @board[row][index][0]
+        values = @board[row][index][0]
       end
     end
 
-    found_values
+    values
   end
 
 end
+
+solver = Solver.new
+solver.board[0][0] = [5]
+
